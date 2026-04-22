99833ba0a128992f07868d65cc31bbeeed98d452
Fernando Taricano <Fernando.taricano@cpzseg.com.br>
2026-04-22 17:40:16 -0300

    Fix lint and empty-test baseline

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {},
+  },
+];
